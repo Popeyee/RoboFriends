b99437a19b0e6ed51288b57d41cbf553c8faee9b
Alii_ <33094586+Popeyee@users.noreply.github.com>
2020-04-24 14:06:15 -0400

    Final project

---
+++ build/precache-manifest.6458d2efb91241bf9e3058f663aa9d1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a9fa1ed0140c862e5c430d4d303162",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "a36264c5169ed8dbafc8",
+    "url": "/static/css/main.a4da2c98.chunk.css"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36264c5169ed8dbafc8",
+    "url": "/static/js/main.a59f6bcc.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
