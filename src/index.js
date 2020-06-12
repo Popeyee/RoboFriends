ac412ec76643a21f8ef39532b62f28f5b19a12ed
Alii_ <33094586+Popeyee@users.noreply.github.com>
2020-06-12 12:57:31 -0400

    Project update: Redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { searchRobots, requestRobots } from './reducers';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import App from './containers/App';
 import './index.css';
-import App from './containers/App'
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
 
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+
+const store = 
+	createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render( 
-			<App />
-			,document.getElementById('root'));
+			<Provider store ={store} >
+				<App />
+			</Provider>,document.getElementById('root'));
+
+
 
 serviceWorker.register();
+
+
+
